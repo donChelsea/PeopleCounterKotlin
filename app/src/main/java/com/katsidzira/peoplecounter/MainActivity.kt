29269533ba5e07c0643cb2e9def43f957988c67b
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2020-10-29 21:32:12 -0400

    text color changing when over capacity

--- app/src/main/java/com/katsidzira/peoplecounter/MainActivity.kt
+++ app/src/main/java/com/katsidzira/peoplecounter/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.katsidzira.peoplecounter
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
@@ -12,45 +10,45 @@ import com.katsidzira.peoplecounter.controller.CounterController
 import com.katsidzira.peoplecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var controller: CounterController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         controller = CounterController
-        mainBinding.controller = controller
+        binding.controller = controller
 
-        mainBinding.buttonAdd.setOnClickListener {
+        binding.buttonAdd.setOnClickListener {
             controller.addPerson()
             updateViews()
         }
 
-        mainBinding.buttonRemove.isInvisible = true
-        mainBinding.buttonRemove.setOnClickListener {
+        binding.buttonRemove.isInvisible = true
+        binding.buttonRemove.setOnClickListener {
             controller.removePerson()
             updateViews()
         }
 
-        mainBinding.buttonReset.setOnClickListener {
+        binding.buttonReset.setOnClickListener {
             controller.reset()
-            if (controller.getPeople() == 0) {
-                mainBinding.buttonRemove.isInvisible = true
-            } else {
-                mainBinding.buttonRemove.isInvisible = true
-            }
             updateViews()
         }
     }
 
     private fun updateViews() {
         if (controller.getPeople()!! > 0) {
-            mainBinding.buttonRemove.isVisible = true
+            binding.buttonRemove.isVisible = true
         } else {
-            mainBinding.buttonRemove.isInvisible = true
+            binding.buttonRemove.isInvisible = true
         }
-        mainBinding.textviewPeople.text = "${controller.getPeople().toString()} people"
-        mainBinding.textviewTotal.text = "Total: ${controller.getTotal().toString()}"
+        if (controller.getPeople()!! >= 15) {
+            binding.textviewPeople.setTextColor(Color.RED)
+        } else {
+            binding.textviewPeople.setTextColor(Color.BLACK)
+        }
+        binding.textviewPeople.text = "${controller.getPeople().toString()} people"
+        binding.textviewTotal.text = "Total: ${controller.getTotal().toString()}"
     }
 }
